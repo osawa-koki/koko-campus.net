ae54f36818377a3b7cf51c2d3618f89a68b5a12e
s-usukura <81461525+s-usukura@users.noreply.github.com>
2022-08-08 09:11:02 +0000

    update: メモ機能を色々修正 #1 #2 #3

--- web/js/S/70.js
+++ web/js/S/70.js
@@ -1,59 +1,69 @@
 "use strict";
 (() => {
 
-    function setVisibility(element, visibility){
-        // 状態をチェックしてから、visibilityをセットする。
-        // visibilityは、"hidden" or "show"
-        if(visibility == "hidden" && !element.classList.contains("hidden")){
-            element.classList.toggle("hidden");
+    const pageInfo = location.href.match(/S(?<Subjects>\d{4})L(?<Lesson>\d{2})P(?<Page>\d{2})/).groups;;
+    const previewButton = document.getElementById("notePreviewButton");
+    const saveButton = document.getElementById("noteSaveButton");
+    const saveButtonIcon = document.getElementById("noteSaveButtonIcon");
+    const noteArea = document.getElementById("noteArea");
+    
+    const [OPEN, CLOSE] = [false, true]; // ノートのプレビュー状態
+    let previewState = CLOSE;
+
+    const [OLD, UPDATE, LATEST] = [0, 1, 2]; // ノートのセーブ状況
+    const toClassName = ["gg-circle-red", "gg-spinner", "gg-circle-green"]
+    let saveState = LATEST;
+    
+    let isAutoSave = true;
+    const saveIntervalMSEC = 1000;
+
+    function updatePreview(previewState){
+        // メモの表示・非表示を切り替える
+        // Args:
+        // - previewState: OPEN or CLOSE
+        
+        function _set(ele, vis){
+            if(ele.style.visibility != vis){
+                ele.style.visibility = vis;
+            }
+        }
+
+        if(previewState == CLOSE) {
+            _set(saveButton, "hidden");
+            _set(noteArea, "hidden");
             return false;
-        }else if(visibility == "show" && element.classList.contains("hidden")){
-            element.classList.toggle("hidden");
+        } else if(previewState == OPEN){
+            _set(saveButton, "visible");
+            _set(noteArea, "visible");
             return false;
-        }else{
+        } else{
             return true;
         }
     }
 
-    const pageInfo = location.href.match(/S(?<Subjects>\d{4})L(?<Lesson>\d{2})P(?<Page>\d{2})/).groups;
-    const noteButton = document.getElementById("noteButton");
-    const updateButton = document.getElementById("noteUpdateButton");
-    const noteArea = document.getElementById("noteArea");
-    let is_latest = true;
 
+    function updateSave(saveState){
+        // セーブ状況を表すアイコンを変更する
+        // Args:
+        // - saveState: OLD, UPDATE or LATEST
 
-    // メモの表示・非表示
-    noteButton.addEventListener("click", function(){
-        noteArea.classList.toggle("hidden");
-        updateButton.classList.toggle("hidden");
-    });
-
-    document.addEventListener("click", (e) => {
-        if(!e.target.closest(".noteBoxContent")) {
-            //noteBoxContent以外の場所をクリックした場合の処理
-            setVisibility(noteArea, "hidden")
-            setVisibility(updateButton, "hidden")
+        const className = toClassName[saveState]
+        if(!saveButtonIcon.classList.contains(className)){
+            saveButtonIcon.className = className;
         }
-    })
+    }
 
 
-    // ノートの内容が変更されたら、is_latestをfalseにする
-    noteArea.addEventListener("input", function(){
-        is_latest = false;
-    });
+    function saveContent(){
+        // メモの内容の更新する関数
 
-
-    // ノートが保存されていなかったら、ページを離れる前に警告を出す
-    window.addEventListener('beforeunload', function(e){
-        if(!is_latest){
-            e.preventDefault();
-            e.returnValue = '';
+        if(saveState != OLD){
+            return
         }
-    });
+        
+        saveState = UPDATE;
+        updateSave(saveState);
 
-
-    // メモの内容の更新
-    updateButton.addEventListener("click", function(){
         const content = noteArea.value;
         const url = "/A";
         const postData = URLencodeAssoc({
@@ -79,50 +89,117 @@
         .then(response => {
             if (response.Success) {
                 // サーバでの処理成功時に実行する処理
-                window.alert("update完了!");
-                is_latest = true;
+                saveState = (saveState == UPDATE) ? LATEST : OLD; // 実行中にsaveStateの状況が変化していたらOLDにする
+                isAutoSave = true;
             } else {
                 window.alert(response.ErrorMessage.join("\n"))
+                saveState = OLD;
+                isAutoSave = false;
             }
         })
-        .catch(() => {
-            window.alert("通信に失敗しました。\n時間を開けて再度登録を行ってください。")
+        .catch((e) => {
+            window.alert("通信に失敗しました。\n時間を開けて再度登録を行ってください。\nメモ欄の上の赤いボタンを押すと、手動で保存を実行できます。")
+            console.log(e)
+            saveState = OLD;
+            isAutoSave = false;
+        })
+        .finally(() => {
+            updateSave(saveState);
         });
+    }
 
+
+    // previewButtonでメモを表示・非表示
+    previewButton.addEventListener("click", function(){
+        previewState = !previewState;
+        updatePreview(previewState);
     });
 
     
-    // ページを読み込んだときに、メモの内容を取得して挿入する。
-    const url = "/A";
-    const postData = URLencodeAssoc({
-        "program" : "subjects",
-        "action" : "notes",
-        "method": "get",
-        "subject": pageInfo["Subjects"],
-        "lesson": pageInfo["Lesson"],
-        "page": pageInfo["Page"],
-    });
-    const data = {
-        method: "POST",
-        mode: "cors",
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/x-www-form-urlencoded",
-        }
-    };
-    data["body"] = postData;
-    fetch(url, data)
-    .then(response => response.json())
-    .then(response => {
-        if (response.Success) {
-            // サーバでの処理成功時に実行する処理
-            noteArea.value = response.Content
-        } else {
-            window.alert(response.ErrorMessage.join("\n"))
+    //noteBoxContent以外の場所をクリックしらメモを閉じる
+    document.addEventListener("click", (e) => {
+        if(!e.target.closest(".noteBoxContent")) {
+            //noteBoxContent以外の場所をクリックした場合の処理
+            previewState = CLOSE;
+            updatePreview(previewState);
         }
     })
-    .catch((ex) => {
-        console.log(ex);
-        window.alert("通信に失敗しました。\n時間を開けて再度登録を行ってください。")
+
+
+    // セーブボタンをクリックで保存
+    saveButton.addEventListener("click", saveContent);
+
+
+    // オートセーブ
+    setInterval(function(){
+        if(isAutoSave){
+            saveContent();
+        }
+    }, saveIntervalMSEC);
+
+
+    // ノートの内容が変更されたとき
+    noteArea.addEventListener("input", function(){
+        saveState = OLD;
+        updateSave(saveState);
+    });
+
+
+    // ノートが保存されていなかったら、ページを離れる前に警告を出す
+    window.addEventListener('beforeunload', function(e){
+        if(saveState != LATEST){
+            e.preventDefault();
+            e.returnValue = 'ノートの内容が保存されていません！';
+        }
+    });
+
+    
+    // ページを読み込んだときの処理
+    window.addEventListener('load', () => {
+
+        updatePreview(previewState);
+        updateSave(saveState);
+        
+        const url = "/A";
+        const postData = URLencodeAssoc({
+            "program" : "subjects",
+            "action" : "notes",
+            "method": "get",
+            "subject": pageInfo["Subjects"],
+            "lesson": pageInfo["Lesson"],
+            "page": pageInfo["Page"],
+        });
+        const data = {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/x-www-form-urlencoded",
+            }
+        };
+        data["body"] = postData;
+        fetch(url, data)
+        .then(response => response.json())
+        .then(response => {
+            if (response.Success) {
+                // サーバでの処理成功時に実行する処理
+                noteArea.value = response.Content
+                saveState = (saveState == UPDATE) ? LATEST : OLD; // 実行中にsaveStateの状況が変化していたらOLDにする 
+                isAutoSave = true;
+            } else {
+                window.alert(response.ErrorMessage.join("\n"))
+                saveState = OLD;
+                isAutoSave = false;
+            }
+        })
+        .catch((ex) => {
+            console.log(ex);
+            window.alert("通信に失敗しました。\n時間を開けて再度登録を行ってください。\nメモ欄の上の赤いボタンを押すと、手動で保存を実行できます。")
+            saveState = OLD;
+            isAutoSave = false;
+        })
+        .finally(() => {
+            updateSave(saveState);
+        });
     });
 })();
