c77f93aa76f1bb73bebb0cc05906617f25200124
koko <koko@koko-campus.org>
2022-08-13 21:37:58 +0900

    DBbackup

--- src/batchDBbackupCreater/batchDBbackupCreater.go
+++ src/batchDBbackupCreater/batchDBbackupCreater.go
@@ -14,9 +14,11 @@ func main() {
 				fileName := fmt.Sprintf("z___%s___%s", GetDate(), table)
 				var SQL SQLbuilder
 				SQL.Add("SELECT * FROM subjects INTO")
-				SQL.Add(fmt.Sprintf("OUTFILE 'C:/webSite/DBbackup/%s.csv'", fileName))
+				SQL.Add(fmt.Sprintf("OUTFILE 'C:/webSite/DBbackup/%s.csv'", fileName)) // HARDCORD!
 				SQL.Add("FIELDS TERMINATED BY ',' ENCLOSED BY '\"' ESCAPED BY '\"' LINES TERMINATED BY '\\r\\n';")
 				Execute(&SQL)
+			} else {
+				fmt.Println(er.Error())
 			}
 		}
 	}
