899072f5a05f9885e38fd15f62e94ff4792b8e00
koko <github@koko-campus.com>
2022-08-28 16:48:30 +0900

    数独SOLVER デバグ用コミット

--- web/js/00/00.js
+++ web/js/00/00.js
@@ -27,13 +27,20 @@ const gtEq = (a, b) => a >= b;
 const lt = (a, b) => a < b;
 const ltEq = (a, b) => a <= b;
 
+const and = (a, b) => a && b;
+const or = (a, b) => a || b;
+const xor = (a, b) => (a || b) && !(a && b);
+const not = a => !a;
+const truthy = a => Boolean(a);
+const falsy = a => !truthy(a);
+
 const apply = fx => arg => fx(arg);
 const rec = fx => fx(fx);
 
 const map = (fx, [a, ...b]) => (a !== undefined) ? [fx(a), ...map(fx, b)] : [];
 const looper = ([a, ...b], fx) => (a !== undefined) ? [fx(a), ...looper(b, fx)] : [];
-const any = ([a, ...b], fx) => (a !== undefined) ? fx(a) || any(b) : false;
-const all = ([a, ...b], fx) => (a !== undefined) ? fx(a) && all(b) : true;
+const any = ([a, ...b], fx) => (a !== undefined) ? or(fx(a), any(b)) : false;
+const all = ([a, ...b], fx) => (a !== undefined) ? and(fx(a), all(b)) : true;
 const countSatisfy = ([a, ...b], fx, i = 0) => (a !== undefined) ? countSatisfy(b, fx, i + (fx(a) ? 1 : 0)) : i;
 
 const getElm = ([a, ...b]) => (a !== undefined) ? [document.getElementById(a), ...getElm(b)] : [];
@@ -41,7 +48,7 @@ const mkElm = ([a, ...b]) => (a !== undefined) ? [document.createElement(a), ...
 const mkElmSVG = ([a, ...b]) => (a !== undefined) ? [document.createElementNS(NAMESPACE_OF_SVG, a), ...mkElmSVG(b)] : [];
 const zFill = (n, len) => (Array(len).join("0") + n).slice(-len);
 const URLencodeAssoc = obj => Object.keys(obj).map(key => key + "=" + encodeURIComponent(obj[key])).join("&");
-const between = (a, b) => c => a <= c.length && c.length <= b;
+const between = (a, b) => c => and(a <= c.length, c.length <= b);
 const switcher = (tf, afx, bfx, arg = null) => (tf) ? afx(arg) : bfx(arg);
 
 const regex = a => b => b.match(a);
@@ -52,13 +59,16 @@ const regexGrouping = (a, b) => a.match(b).groups;
 
 const removeChildren = parent => (parent.firstChild) ? [parent.removeChild(parent.firstChild), removeChildren(parent)] : [];
 
-const append = ([a, ...b], parent) => (a !== undefined) ? [parent.appendChild(a), append(b, parent)] : [];
+const append = ([a, ...b], parent) => (a !== undefined) ? [parent.appendChild(a), ...append(b, parent)] : [];
+const push = ([a, ...b], list) => (a !== undefined) ? [list.push(a), ...push(b, list)] : [];
 
-const doNtimes = (n, fx, i = 0) => (i < n) ? [fx(i), doNtimes(n, fx, i + 1)] : [];
+const doNtimes = (n, fx, i = 0) => (i < n) ? [fx(i), ...doNtimes(n, fx, i + 1)] : [];
 
 const round = n => i => Math.round(i * n) / n;
 const round100 = round(100);
 
 const NxN = (a, b, c = null) => [new Array(a).fill(new Array(b).fill(c))];
+const NxNfx = (a, b, fx) => [new Array(a).fill(new Array(b).fill(fx()))];
 
-
+const fromAtoB = (a, b, step = 1, eq = true, i = 0) => (((eq) ? ltEq : lt)(a + i, b)) ? [a + i, ...fromAtoB(a, b, step, eq, i + step)] : [];
+const mixupMesh = (a, b, i = 0) =>  (i < a.length * b.length) ? [[a[Math.floor(i / b.length)], b[i % b.length]], ...mixupMesh(a, b, i + 1)] : [];
--- web/js/P/0018.js
+++ web/js/P/0018.js
@@ -1,4 +1,58 @@
+"use strict";
 
-function
+const ITEM_COUNT = 9;
+
+const itemBoxes = [];
+
+
+const rowcolumn2index = (row, column) => row * ITEM_COUNT + column;
+const index2rowcolumn = index => [index % ITEM_COUNT, Math.floor(index / ITEM_COUNT)];
+const whereAmI_index = myself => itemBoxes.indexOf(myself);
+const whereAmI_rowcolumn = myself => index2rowcolumn(whereAmI_index(myself));
+const obtainObjectFromRowColumn = (row, column) => itemBoxes[rowcolumn2index(row, column)];
+const obtainValueFromRowColumn = (row, column) => itemBoxes[rowcolumn2index(row, column)].value;
+const obtainAllIndexesOfRow = row => fromAtoB(row * ITEM_COUNT, row * ITEM_COUNT + ITEM_COUNT - 1);
+const obtainAllIndexesOfColumn = column => fromAtoB(column, ITEM_COUNT ** 2, ITEM_COUNT);
+const obtainAllIndexesOfBlock = (row, column) => mixupMesh(fromAtoB(Math.floor(row / 3) * 3), fromAtoB(Math.floor(column / 3) * 3));
+
+const validationCheck = (array_9x9, [row, column], i) => (rowCheck(array_9x9, row, i) && columnCheck(array_9x9, column, i) && blockCheck(array_9x9, [row, column], i));
+const rowCheck = (array_9x9, row, i) => countSatisfy(obtainAllIndexesOfRow(row), index => array_9x9[index].value == i) === 1;
+const columnCheck = (array_9x9, column, i) => countSatisfy(obtainAllIndexesOfColumn(column), index => array_9x9[index].value == i) === 1;
+const blockCheck = (array_9x9, [row, column], i) => countSatisfy(obtainAllIndexesOfBlock(row, column), index => array_9x9[index].value == i) === 1;
+
+
+(() => { // init
+	const items = doNtimes(ITEM_COUNT ** 2, () => {
+		const [item] = mkElm(["input"]);
+		item.type = "number";
+		item.maxLength = "1";
+		item.addEventListener("input", inputCheck);
+		return item;
+	});
+	const [itemContainer] = getElm(["sudokuBox"]);
+	append(items, itemContainer);
+	push(items, itemBoxes);
+})();
+
+function inputCheck() {
+	const numbers = map(input => input.value || 0, itemBoxes);
+	console.log(numbers);
+	if (!validationCheck(numbers, index2rowcolumn(whereAmI_index(this)))) {
+		console.log(this);
+		return;
+	}
+	console.log("OK");
+}
+
+
+document.getElementById("button").addEventListener("click", function() {
+	
+})
+
+
+
+
+
+
 
 
