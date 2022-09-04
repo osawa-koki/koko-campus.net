c1c6d6ff37298e734c5af99d357fd19d8ef8debb
koko <github@koko-campus.com>
2022-09-04 21:31:34 +0900

    開発者用情報ファイルを更新。

--- dev/deployTool/installMySQL.bash
+++ dev/deployTool/installMySQL.bash
@@ -2,7 +2,6 @@
 sudo apt install mysql-server
 
 sudo mysql
-CREATE USER 'koko'@'localhost' IDENTIFIED WITH mysql_native_password BY 'BZEVIM&-10-gCi-J&ZG6Peq!619RUkozJQc0vYZdQO#ZADC1r16l70gQCr7STP!ahmWwdrfHjP3Pu#PVXMu1n2eBubu$WCIBcJT?';
+CREATE USER 'koko'@'localhost' IDENTIFIED WITH mysql_native_password BY 'pw1234';
 
 mysql_secure_installation
-
