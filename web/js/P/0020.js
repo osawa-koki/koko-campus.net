6f13f4938bb5944d77b1f5dfa83f47efa503c894
koko <github@koko-campus.com>
2022-09-04 17:42:38 +0900

    バイナリエディタ utf8まで完成。

--- web/js/P/0020.js
+++ web/js/P/0020.js
@@ -6,22 +6,29 @@ const DEC = 10;
 const HEX = 16;
 const FILE_SIZE_CONFIRM_LIMIT = 10 << 20;
 
-const [importImg, exportImg, fileButton, fileName, editerX, editerY, loading] = getElm(["importImg", "exportImg", "fileButton", "fileName", "editerX", "editerY", "loading"]);
+const byteObjects = [];
+const makeByteObject = (index, uint8, doc) => ({
+	index: index,
+	uint8: uint8,
+	doc: doc,
+});
 
+const [importImg, exportImg, fileButton, fileName, editerX, editerY, loading] = getElm(["importImg", "exportImg", "fileButton", "fileName", "editerX", "editerY", "loading"]);
+const [x_uint8, x_bit, x_utf8, x_utf16, x_sjis, x_eucjp] = getElm(["x_uint8", "x_bit", "x_utf8", "x_utf16", "x_sjis", "x_eucjp"]);
 
 function importFile() {
 	fileButton.click();
 }
 
 fileButton.addEventListener("change", function() {
-	const file = fileButton.files[0]; // ★ fst(fileButton.files)
+	const file = fst(fileButton.files);
 	const reader = new FileReader();
 	reader.readAsArrayBuffer(file);
 	reader.onload = () => {
+		byteObjects.splice(0);
 		loading.classList.add("loading");
-		exportImg.classList.remove("unable");
+		//exportImg.classList.remove("unable");
 		removeChildren(editerX);
-		removeChildren(editerY);
 		const dataView = new DataView(reader.result);
 		if (FILE_SIZE_CONFIRM_LIMIT < dataView.byteLength && !window.confirm(`ファイルサイズが「${dataView.byteLength}」バイトあります。\n処理を続行しますか???`)) {
 			loading.classList.remove("loading");
@@ -40,7 +47,9 @@ function readOutByte(dataView, i) {
 	const [hexElement] = mkElm(["input"]);
 	hexElement.type = "text";
 	hexElement.classList.add("hex");
-	hexElement.value = dataView.getUint8(i).toString(HEX).toUpperCase().padStart(2, "0");
+	const uint8 = dataView.getUint8(i);
+	hexElement.value = uint8.toString(HEX).toUpperCase().padStart(2, "0");
+	byteObjects.push(makeByteObject(i, uint8, hexElement));
 	hexElement.addEventListener("input", function() {
 		const value = this.value;
 		this.value = value.replace(/[^a-fA-F0-9]/, "").slice(-2).toUpperCase();
@@ -50,6 +59,7 @@ function readOutByte(dataView, i) {
 		setTimeout(() => { // FIX ME
 			this.setSelectionRange(length, length);
 		}, 10);
+		showDetail(this);
 	});
 	editerX.appendChild(hexElement);
 	loading.classList.remove("loading");
@@ -58,6 +68,31 @@ function readOutByte(dataView, i) {
 importImg.addEventListener("click", importFile);
 
 
-
-
-
+function showDetail(me) {
+	const myObject = finder(byteObjects, byteObject => byteObject.doc === me);
+	x_uint8.textContent = myObject.uint8;
+	x_bit.textContent = myObject.uint8.toString(BIN).padStart(8, "0");
+	x_utf8.textContent = (() => {
+		const charsStruct = {
+			chars: [],
+			start: null,
+		};
+		for (let i = myObject.index; 0 <= i; i--) {
+			const bin = byteObjects[i].uint8.toString(BIN).padStart(8, "0");
+			if (bin.match(/^0/)) {
+				const textDecoder = new TextDecoder("utf-8");
+				return textDecoder.decode(Uint8Array.from([byteObjects[i].uint8]).buffer);
+			};
+			if (!bin.match(/^11/)) continue;
+			charsStruct.start = i;
+			const byteLen = fst(bin.match(/^1+/)).length;
+			doNtimes(byteLen, n => {
+				const position = i + n;
+				charsStruct.chars.push(byteObjects[position].uint8);
+			});
+			break;
+		}
+		const textDecoder = new TextDecoder("utf-8");
+		return textDecoder.decode(Uint8Array.from(charsStruct.chars).buffer);
+	})();
+}
