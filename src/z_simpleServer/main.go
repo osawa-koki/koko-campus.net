11ebfeb4145a6e38f8863b4307b20e39d3c373e9
koko <github@koko-campus.com>
2022-09-08 17:23:24 +0900

    pyAPI用のテストデータを作成。

--- src/z_simpleServer/main.go
+++ src/z_simpleServer/main.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
 func controller(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "this is a test.")
+	var answer string
+
+	bodyByte, _ := io.ReadAll(r.Body)
+	body := string(bodyByte)
+
+	w.Header().Set("Content-Type", "text/plain")
+
+	answer += "path -> " + r.URL.Path + "\n"
+	answer += "query -> " + r.URL.RawQuery + "\n"
+	answer += "body -> " + body + "\n"
+	fmt.Fprint(w, answer)
 }
 
 func main() {
 	http.HandleFunc("/", controller)
-	if er := http.ListenAndServe("", nil); er != nil {
+	if er := http.ListenAndServe(":8888", nil); er != nil {
 		fmt.Println("ListenAndServeに失敗")
 		fmt.Println(er.Error())
 	}
