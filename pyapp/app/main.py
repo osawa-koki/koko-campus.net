04a2259acbde4a49a639043f061d4a4ec796ed13
s-usukura <81461525+s-usukura@users.noreply.github.com>
2022-09-30 13:33:35 +0000

    本番環境では/docs,/redcを非表示

--- pyapp/app/main.py
+++ pyapp/app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from . import database
 from .routers import emoji, note
-
+from .config import settings
 
 app = FastAPI()
+
+if not settings.DEBUG:
+    app = FastAPI(docs_url=None, redoc_url=None)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
