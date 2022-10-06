b3dcd13da65ad47862bb85923b62557482cf088d
koko <github@koko-campus.net>
2022-10-06 20:54:37 +0900

    アイコン画像登録処理。

--- src/webServer/APImypage.go
+++ src/webServer/APImypage.go
@@ -33,7 +33,7 @@ func mypageAPI(RR *RequestResponse) string {
 		if fhd.Size <= iconFileSizeLimit {
 			if src, er := fhd.Open(); er == nil {
 				defer src.Close()
-				if dst, er := os.Create(getRootDIR() + "/img/M00/" + userId + ".png"); er == nil {
+				if dst, er := os.Create(getRootDIR() + "../static/img/M00/" + userId + ".png"); er == nil {
 					defer dst.Close()
 					io.Copy(dst, src)
 					success = true
