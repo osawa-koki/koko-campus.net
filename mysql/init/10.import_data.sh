4db3432061588f4828990ff1f66155831004da82
osawa-koki <github@osawa.dev>
2023-01-12 02:25:56 +0900

    ゲームデータ。

--- mysql/init/10.import_data.sh
+++ mysql/init/10.import_data.sh
@@ -4,4 +4,4 @@
 mysql -u root --password=root_password --local-infile koko_campus -e "LOAD DATA LOCAL INFILE '/master-data/category.csv' INTO TABLE category FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 LINES"
 
 # # games
-# mysql -u root --password=root_password --local-infile koko_campus -e "LOAD DATA LOCAL INFILE '/master-data/games.csv' INTO TABLE games FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n'"
+mysql -u root --password=root_password --local-infile koko_campus -e "LOAD DATA LOCAL INFILE '/master-data/games.csv' INTO TABLE games FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 LINES"
