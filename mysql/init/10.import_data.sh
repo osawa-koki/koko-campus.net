943cbf3b1c6596f1f16ade65921c009a9f053ef5
osawa-koki <github@osawa.dev>
2023-01-12 02:25:05 +0900

    先頭一行は無視。

--- mysql/init/10.import_data.sh
+++ mysql/init/10.import_data.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 
 # category
-mysql -u root --password=root_password --local-infile koko_campus -e "LOAD DATA LOCAL INFILE '/master-data/category.csv' INTO TABLE category FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n'"
+mysql -u root --password=root_password --local-infile koko_campus -e "LOAD DATA LOCAL INFILE '/master-data/category.csv' INTO TABLE category FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 LINES"
 
 # # games
 # mysql -u root --password=root_password --local-infile koko_campus -e "LOAD DATA LOCAL INFILE '/master-data/games.csv' INTO TABLE games FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n'"
