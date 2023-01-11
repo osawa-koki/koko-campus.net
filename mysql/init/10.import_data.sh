a216ace7007477e0ffae18b357184da09b94504d
osawa-koki <github@osawa.dev>
2023-01-12 02:23:51 +0900

    文字化け問題直った。

--- mysql/init/10.import_data.sh
+++ mysql/init/10.import_data.sh
@@ -3,5 +3,5 @@
 # category
 mysql -u root --password=root_password --local-infile koko_campus -e "LOAD DATA LOCAL INFILE '/master-data/category.csv' INTO TABLE category FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n'"
 
-# games
-mysql -u root --password=root_password --local-infile koko_campus -e "LOAD DATA LOCAL INFILE '/master-data/games.csv' INTO TABLE games FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n'"
+# # games
+# mysql -u root --password=root_password --local-infile koko_campus -e "LOAD DATA LOCAL INFILE '/master-data/games.csv' INTO TABLE games FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n'"
